673ad041104aedc072e5dfad5b6b2b56cbaa652b
njchiang <jeff.njchiang@gmail.com>
2019-08-10 16:57:21 -0700

    move submodule back to properly move

--- analysis/fmri/utils/README.md
+++
@@ -1,4 +0,0 @@
-# task-fmri-utils
-
-RETOOLING FOR HOFFMAN USE... STAY TUNED...
-
--- analysis/fmri/utils/fmri_core/__init__.py
+++
@@ -1,4 +0,0 @@
-# from fmri_core import analysis as analysis
-# from fmri_core import utils as utils
-# from fmri_core import vis as vis
-# from fmri_core import pype as pype
