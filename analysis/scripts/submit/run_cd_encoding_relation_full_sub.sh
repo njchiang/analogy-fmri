9c0e1c2878ad3e5357a8903e4725a2fe7d476a92
njchiang <jeff.njchiang@gmail.com>
2019-11-10 09:35:10 -0800

    add subject run

---
+++ analysis/scripts/submit/run_cd_encoding_relation_full_sub.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+:<<doc
+qsub \
+    -o /u/project/monti/njchiang/code/analogy/jobs/output/ \
+    -e /u/project/monti/njchiang/code/analogy/jobs/output/ \
+    -V -N ab-encoding \
+    -l h_data=8G,h_rt=23:59:59 -pe shared 2 \
+    -M ${USER} -m bea \
+    /u/project/monti/njchiang/code/analogy/analogy-fmri/analysis/scripts/submit/run_ab_encoding.sh
+
+doc
+
+echo "Script started"
+. /u/home/n/njchiang/.bashrc
+conda activate fmri
+echo "environment activated"
+returnHere=${PWD}
+cd /u/project/monti/njchiang/code/analogy/analogy-fmri
+python analysis/run_encoding.py \
+  --phase=CDMatch \
+  --cv=relation \
+  --n_folds=4 \
+  --permutations=0 \
+  --alpha=0.1 \
+  --subject=${1}
+  # --n_folds=4 \
+cd ${returnHere}
